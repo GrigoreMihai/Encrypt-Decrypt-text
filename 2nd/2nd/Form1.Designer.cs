77aaa957e12daa52b62e5d06415cfbc8211ff21d
GrigoreMihai <40674950+GrigoreMihai@users.noreply.github.com>
2019-05-16 07:53:23 +0300

    Add files via upload

---
+++ 2nd/2nd/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace _2nd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.B1 = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(294, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // B
+            // 
+            this.B.Enabled = false;
+            this.B.Location = new System.Drawing.Point(39, 60);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(75, 23);
+            this.B.TabIndex = 1;
+            this.B.Text = "Crypt";
+            this.B.UseVisualStyleBackColor = true;
+            this.B.Click += new System.EventHandler(this.B_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(346, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(111, 97);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // B1
+            // 
+            this.B1.Enabled = false;
+            this.B1.Location = new System.Drawing.Point(185, 60);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(75, 23);
+            this.B1.TabIndex = 3;
+            this.B1.Text = "Decrypt";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(231, 262);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 4;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(497, 313);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Aplicatie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button B;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button ClearButton;
+    }
+}
+
---
+++ 2nd/2nd/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2nd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Introduceti textul si alegeti optiunea dorita "); 
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            B.Enabled = true;
+            B1.Enabled = true;
+           
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            //criptarea
+            
+
+            char[] c; string a;  int i;
+            a = textBox1.Text;
+            c = a.ToCharArray();
+            int[] v = new int[300];
+            for (i = 0; i < c.Length; i++)
+                v[i] = (int)c[i];            
+                for (i = 0; i < c.Length; i++)                   
+                        v[i] = v[i] * 3 + 5;
+                   
+            for (i = 0; i < c.Length; i++)
+                textBox2.Text += v[i];
+     
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void B1_Click(object sender, EventArgs e)
+        {
+            //decriptarea
+           
+            char[] c; string a;  int[] v = new int[300];
+                a = textBox1.Text;
+                c = a.ToCharArray();
+                int i;
+                for (i = 0; i < c.Length; i++)
+                    v[i] = (int)Char.GetNumericValue(c[i]);                
+                    for (i = 0; i < c.Length; i += 3)                          
+                                v[i] = (v[i] * 100 + v[i + 1] * 10 + v[i + 2] - 5) / 3;
+                           
+                                
+                  
+
+                for (i = 0; i < c.Length; i+=3)
+                    textBox2.Text += Convert.ToChar(v[i]);
+                
+                
+                
+            }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+    
+        
+
+            
+
+
+        }
+
+
+
+    
+}
